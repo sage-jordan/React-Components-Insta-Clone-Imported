9695835912f73bab29333e46266c6e3125890265
sage-jordan <sagemjordan@gmail.com>
2020-06-09 11:56:06 -0400

    added Like functionality

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -9,7 +9,7 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
-  const likes = useState(props.likes)
+  const [likes, setLikes] = useState(props.post.likes)
   console.log(props)
 
   return (
@@ -27,7 +27,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection likes={likes} setLikes={setLikes} />
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
